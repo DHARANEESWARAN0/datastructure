7b71a5c0a4c9556a2383b3d2968f1aac40cd4996
DHARANEESWARAN0 <dharaneeswaran792@gmail.com>
2025-03-21 14:31:56 +0530

    mk

--- Main.java
+++ Main.java
@@ -1,10 +1,18 @@
-public class Main
-{
-	public static void main(String[] args) {
-		int[] arr={1,2,3,4,5};
-		int n=arr.length;
-		for(int i=0;i<n;i++){
-		    System.out.print(arr[i]);
-		}
-	}
-}	
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for (int i=0;i<n;i++) {
+            arr[i]=sc.nextInt();
+        }
+        int newElement=sc.nextInt();
+        int[] newArr=new int[n + 1];
+        newArr[0]=newElement;
+        for (int i=0;i<n;i++) {
+            newArr[i + 1]=arr[i];
+        }
+        System.out.println(Arrays.toString(newArr));
+    }
+}
